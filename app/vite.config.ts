8a18ead920aae46b5b0b241bd0e5ade2c86f5931
jakubkarp <quni.inne@gmail.com>
2026-02-18 00:58:57 +0100

    feat: buffer for bip39

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -2,10 +2,22 @@ import path from 'path';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react(), tailwindcss()],
+    plugins: [
+        react(),
+        tailwindcss(),
+        nodePolyfills({
+            globals: {
+                Buffer: true,
+                global: true,
+                process: true,
+            },
+            protocolImports: true,
+        }),
+    ],
     resolve: {
         alias: {
             '@': path.resolve(__dirname, './src'),
