8fcd95bb44b3771823602cd5df3fc6457ffb8887
Jakub Karp <80711470+fn-jakubkarp@users.noreply.github.com>
2026-02-21 02:25:14 +0100

    Logo + initial SEO (#6)
    * chore: add svgs
    
    * chore: update Logo.tsx
    
    * feat: seo
    
    * fix: logo
    
    * fix: theme-provider

--- app/src/components/base/Logo/Logo.tsx
+++ app/src/components/base/Logo/Logo.tsx
@@ -1,5 +1,6 @@
 import { useNavigate } from 'react-router-dom';
 import Typography from '../Typography/typography';
+import { useTheme } from '@/components/theme-provider';
 
 interface LogoProps {
     navigate?: boolean;
@@ -7,20 +8,27 @@ interface LogoProps {
 
 export const Logo = ({ navigate }: LogoProps) => {
     const nav = useNavigate();
+    const { resolvedTheme } = useTheme();
+
+    const logoSrc =
+        resolvedTheme === 'dark' ? '/Logo-FFF.svg' : '/Logo-F50.svg';
 
     const logoContent = (
-        <Typography
-            weight='semibold'
-            text='sm'
-            tracking='tight'
-            font='mono'
-            color='foreground'
-            as='span'
-            transform='uppercase'
-            className='leading-none'
-        >
-            CORESEND
-        </Typography>
+        <div className='flex items-center gap-3'>
+            <img src={logoSrc} alt='Coresend' className='h-6 w-auto' />
+            <Typography
+                weight='semibold'
+                text='sm'
+                tracking='tight'
+                font='mono'
+                color='foreground'
+                as='span'
+                transform='uppercase'
+                className='leading-none'
+            >
+                CORESEND
+            </Typography>
+        </div>
     );
 
     if (navigate) {
