34627cc99cbbe00ac7c8a8b8203cd38d02f58b0e
Jakub Karp <80711470+fn-jakubkarp@users.noreply.github.com>
2026-03-07 11:23:46 +0100

    Merge pull request #17 from lirinlabs/terraform
    Terraform and Docker

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,8 +1,16 @@
-variable "tenancy_ocid" {}
-variable "user_ocid" {}
-variable "fingerprint" {}
-variable "private_key_path" {}
-variable "region" { default = "eu-frankfurt-1" } 
-variable "compartment_ocid" {}
-variable "ssh_public_key" {}
-variable "instance_shape" { default = "VM.Standard.A1.Flex" } # ARM (Always Free)
+variable "instance_shape" { default = "VM.Standard.A1.Flex" } 
+variable "compartment_id" {
+  description = "The OCID of your OCI compartment/tenancy"
+  type        = string
+}
+
+variable "region" {
+  description = "The OCI region where resources will be created"
+  type        = string
+  default     = "eu-frankfurt-1" 
+}
+
+variable "ssh_public_key" {
+  description = "The public SSH key to access instances"
+  type        = string
+}
